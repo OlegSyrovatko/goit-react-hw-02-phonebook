42ec8d20b6cc89305e869d5a38f6d269e63ce91e
Oleg <sirov@ukr.net>
2023-04-13 00:55:15 +0300

    class filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Filter = ({ value, onChange }) => (
-  <>
-    Find contacts by name
-    <input
-      type="text"
-      name="filter"
-      value={value}
-      // value={this.state.filter}
-      onChange={onChange}
-      // onChange={this.handleChange}
-    ></input>
-  </>
-);
+class Filter extends Component {
+  state = {
+    filter: '',
+  };
+
+  handleChange = e => {
+    const { value } = e.currentTarget;
+    this.setState({ filter: value });
+    this.props.onChange(e);
+  };
+
+  render() {
+    const { filter } = this.state;
+    return (
+      <>
+        Find contacts by name
+        <input
+          type="text"
+          name="filter"
+          value={filter}
+          onChange={this.handleChange}
+        />
+      </>
+    );
+  }
+}
+
+// import React from 'react';
+
+// const Filter = ({ value, onChange }) => (
+//   <>
+//     Find contacts by name
+//     <input
+//       type="text"
+//       name="filter"
+//       value={value}
+//       // value={this.state.filter}
+//       onChange={onChange}
+//       // onChange={this.handleChange}
+//     ></input>
+//   </>
+// );
 
 export default Filter;
